d691221bc214a391053daed923c171d45475ff5c
red <red@adby.me>
2014-02-24 00:04:54 +0900

    Column schema draft.

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,27 @@ class Test < HbaseRecord::Base
   # self.table_name = "signals"
 
   # 7febbc4758b5e370
+
+  column :cf1 do
+    column :sec do
+      field :third, :string
+    end
+
+    field :two, :string
+  end
+
+  # column :cf1 do
+  #   field :sec, :string
+  #   field :two, :string
+  # end
+
+  column :cf2 do
+    column :sec do
+      field :third, :string
+    end
+  end
+
 end
+
+# Test.get("key").cf1.sec.third => Value
+# Test.get("key").cf1.two => Value
