fa5fce5902c3c09d640f27b7a9353b18d22fcb5a
red <red@adby.me>
2014-02-24 13:56:15 +0900

    Remove draft schema in kona_signal.

--- app/models/kona_signal.rb
+++ app/models/kona_signal.rb
@@ -3,16 +3,16 @@ class KonaSignal < HbaseRecord::Base
 
   # 7febbc4758b5e370
 
-  column_family :abc do
-    column :xx do
-      cell :name, :string
-    end
-    cell :name
-  end
+  # column_family :abc do
+  #   column :xx do
+  #     cell :name, :string
+  #   end
+  #   cell :name
+  # end
 
-  column_family :signal do
-    cell :mac, :string
-    cell :quality, :short
-  end
+  # column_family :signal do
+  #   cell :mac, :string
+  #   cell :quality, :short
+  # end
 
 end
