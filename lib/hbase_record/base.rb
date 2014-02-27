324e8624c5de81fc61cda845e8f47171574b5c5f
nicola <nicola@adby.me>
2014-02-27 06:16:40 +0000

    fix

--- lib/hbase_record/base.rb
+++ lib/hbase_record/base.rb
@@ -1,7 +1,17 @@
+require 'hbase_record/cell'
+require 'hbase_record/columns'
+require 'hbase_record/config'
+require 'hbase_record/connection'
+require 'hbase_record/finder'
+require 'hbase_record/mutation'
+require 'hbase_record/row'
+require 'hbase_record/scope'
+require 'hbase_record/table'
 require 'hbase_record/thrift/hbase_types'
 require 'hbase_record/thrift/hbase_constants'
 require 'hbase_record/thrift/hbase'
 require 'hbase_record/schema/table'
+
 
 module HbaseRecord
   class Base
